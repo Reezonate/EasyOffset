06674b3f89a1d1bd4a73defafc6e9228f3c13767
Reezonate <xxxx359xxxx@gmail.com>
2023-01-18 22:23:43 +0900

    Moved gizmos to MainMenu scene

--- 2_Core/Installers/OnAppInstaller.cs
+++ 2_Core/Installers/OnAppInstaller.cs
@@ -4,38 +4,6 @@ using Zenject;
 namespace EasyOffset.Installers {
     [UsedImplicitly]
     public class OnAppInstaller : Installer<OnAppInstaller> {
-        public override void InstallBindings() {
-            BindInputManagers();
-            BindGizmosManager();
-            BindBenchmarkManager();
-            BindAdjustmentModeManagers();
-        }
-
-        private void BindInputManagers() {
-            Container.BindInterfacesAndSelfTo<ReeInputManager>().AsSingle();
-        }
-
-        private void BindGizmosManager() {
-            Container.BindInterfacesAndSelfTo<GizmosManager>().AsSingle();
-            Container.BindExecutionOrder<GizmosManager>(1);
-        }
-
-        private void BindBenchmarkManager() {
-            Container.BindInterfacesAndSelfTo<SwingBenchmarkManager>().AsSingle();
-            Container.BindExecutionOrder<SwingBenchmarkManager>(1);
-        }
-
-        private void BindAdjustmentModeManagers() {
-            Container.BindInterfacesAndSelfTo<AdjustmentBlockManager>().AsSingle();
-
-            Container.BindInterfacesAndSelfTo<BasicAdjustmentModeManager>().AsSingle();
-            Container.BindInterfacesAndSelfTo<PositionAdjustmentModeManager>().AsSingle();
-            Container.BindInterfacesAndSelfTo<RotationAdjustmentModeManager>().AsSingle();
-            Container.BindInterfacesAndSelfTo<SwingBenchmarkAdjustmentModeManager>().AsSingle();
-            Container.BindInterfacesAndSelfTo<PositionAutoAdjustmentModeManager>().AsSingle();
-            Container.BindInterfacesAndSelfTo<RotationAutoAdjustmentModeManager>().AsSingle();
-            Container.BindInterfacesAndSelfTo<RoomOffsetAdjustmentModeManager>().AsSingle();
-            Container.BindInterfacesAndSelfTo<DirectAdjustmentModeManager>().AsSingle();
-        }
+        public override void InstallBindings() { }
     }
 }
--- 2_Core/Installers/OnMenuInstaller.cs
+++ 2_Core/Installers/OnMenuInstaller.cs
@@ -7,6 +7,16 @@ namespace EasyOffset.Installers {
         [Inject] [UsedImplicitly] private MenuPlayerController _menuPlayerController;
 
         public override void InstallBindings() {
+            BindInputManager();
+            BindGizmosManager();
+            BindBenchmarkManager();
+            BindAdjustmentModeManagers();
+            
+            BindAbomination();
+            BindUI();
+        }
+
+        private void BindAbomination() {
             var vrControllers = new VRControllers(
                 _menuPlayerController.rightController,
                 _menuPlayerController.leftController
@@ -14,11 +24,39 @@ namespace EasyOffset.Installers {
 
             Container.BindInstance(vrControllers).AsSingle();
             Container.BindInterfacesAndSelfTo<AbominationTransformManager>().AsSingle();
-            
-            Container.BindInterfacesAndSelfTo<UIManager>().AsSingle();
+        }
 
+        private void BindUI() {
+            Container.BindInterfacesAndSelfTo<UIManager>().AsSingle();
             Container.Bind<UserGuideViewController>().FromNewComponentAsViewController().AsSingle();
             Container.BindInterfacesAndSelfTo<UserGuideFloatingManager>().AsSingle();
+        }
+
+        private void BindInputManager() {
+            Container.BindInterfacesAndSelfTo<ReeInputManager>().AsSingle();
+        }
+
+        private void BindGizmosManager() {
+            Container.BindInterfacesAndSelfTo<GizmosManager>().AsSingle();
+            Container.BindExecutionOrder<GizmosManager>(1);
+        }
+
+        private void BindBenchmarkManager() {
+            Container.BindInterfacesAndSelfTo<SwingBenchmarkManager>().AsSingle();
+            Container.BindExecutionOrder<SwingBenchmarkManager>(1);
+        }
+
+        private void BindAdjustmentModeManagers() {
+            Container.BindInterfacesAndSelfTo<AdjustmentBlockManager>().AsSingle();
+
+            Container.BindInterfacesAndSelfTo<BasicAdjustmentModeManager>().AsSingle();
+            Container.BindInterfacesAndSelfTo<PositionAdjustmentModeManager>().AsSingle();
+            Container.BindInterfacesAndSelfTo<RotationAdjustmentModeManager>().AsSingle();
+            Container.BindInterfacesAndSelfTo<SwingBenchmarkAdjustmentModeManager>().AsSingle();
+            Container.BindInterfacesAndSelfTo<PositionAutoAdjustmentModeManager>().AsSingle();
+            Container.BindInterfacesAndSelfTo<RotationAutoAdjustmentModeManager>().AsSingle();
+            Container.BindInterfacesAndSelfTo<RoomOffsetAdjustmentModeManager>().AsSingle();
+            Container.BindInterfacesAndSelfTo<DirectAdjustmentModeManager>().AsSingle();
         }
     }
 }
